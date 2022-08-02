b4d970739391684e81a91ae8786ebd1a69a85de3
rosedelaube <ws1004@icloud.com>
2022-08-02 15:39:42 +0900

    in progress

---
+++ cs_Motor_RS485/view/Current_State.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace cs_Motor_RS485.view
+{
+    /// <summary>
+    /// Current_State.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Current_State : Window
+    {
+        public Current_State()
+        {
+            InitializeComponent();
+        }
+    }
+}
